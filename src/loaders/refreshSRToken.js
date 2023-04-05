19fc9e58b8e0ea0be1e5fee3c3d332be1722f4f6
Himanshu Walimbe <himanshuwalimbe@gmail.com>
2023-04-06 02:06:30 +0530

    get token on startup

--- src/loaders/refreshSRToken.js
+++ src/loaders/refreshSRToken.js
@@ -3,28 +3,36 @@ import axios from 'axios'
 //Shiprocket Bearer Token is valid for 10 days. Needs to be refreshed
 
 const refreshSRToken = async (container, options) => {
-  //const refreshDuration = options.refreshDuration
+  //on startup
+  await axios
+    .post('https://apiv2.shiprocket.in/v1/external/auth/login', {
+      email: options.email,
+      password: options.password,
+    })
+    .then((res) => {
+      options.token = res.data.token
+    })
+    .catch((err) => {
+      console.log('Shiprocket token refresh failed')
+      throw err
+    })
 
+  //Everyday at 12am
   const jobSchedulerService = container.resolve('jobSchedulerService')
-  jobSchedulerService.create(
-    'refreshSRToken',
-    {},
-    '0 * * * 0', //Cron Job: Every Sunday at 12am
-    async () => {
-      axios
-        .post('https://apiv2.shiprocket.in/v1/external/auth/login', {
-          email: options.email,
-          password: options.password,
-        })
-        .then((res) => {
-          options.token = res.token
-        })
-        .catch((err) => {
-          console.log('Shiprocket token refresh failed')
-          throw err
-        })
-    }
-  )
+  jobSchedulerService.create('refreshSRToken', {}, '0 0 * * *', async () => {
+    axios
+      .post('https://apiv2.shiprocket.in/v1/external/auth/login', {
+        email: options.email,
+        password: options.password,
+      })
+      .then((res) => {
+        options.token = res.data.token
+      })
+      .catch((err) => {
+        console.log('Shiprocket token refresh failed')
+        throw err
+      })
+  })
 }
 
 export default refreshSRToken
